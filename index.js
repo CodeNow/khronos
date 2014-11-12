33498a9e3f4c104a52acc3ca40dbca86c8db689d
Casey Flynn <cflynn.us@gmail.com>
2014-11-12 13:02:55 -0800

    rename script

--- index.js
+++
@@ -1,3 +0,0 @@
-#!/usr/local/bin/node
-
-var dockerode = require('dockerode');
---
+++ prune-containers.js
@@ -0,0 +1,11 @@
+#!/usr/local/bin/node
+
+var Docker = require('dockerode');
+
+console.log('connecting to docker daemon');
+var docker = new Docker({host:'127.0.0.1', port:4243});
+
+console.log('fetching containers');
+docker.listContainers(function (err, containers) {
+  console.log(err, containers);
+});
