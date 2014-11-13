f915f27d15c2c45915b920f4561052b0c6a9f91f
Casey Flynn <cflynn.us@gmail.com>
2014-11-12 16:37:58 -0800

    use optimist and command line to pass flag for delete time

--- scripts/prune-containers.js
+++ scripts/prune-containers.js
@@ -1,20 +1,38 @@
 #!/usr/bin/env node
-
 /**
  * Prunes containers running/dead > 12 hours
  */
-
+var async = require('async');
 var Docker = require('dockerode');
+var argv = require('optimist').argv;
 
-console.log('connecting to docker daemon');
-try {
-  var docker = new Docker({host:'127.0.0.1', port:4243});
-} catch (e) {
-  console.log('error connecting to docker daemon');
-  throw e;
+var MAX_TIME_CONTAINER = 60 * 60 * 12; // 12 hours
+
+if (typeof argv.t !== 'undefined') {
+  MAX_TIME_CONTAINER = 60*60*parseInt(argv.t);
 }
 
+console.log(argv.t);
+
+console.log('connecting to docker daemon');
+var docker = new Docker({host:'127.0.0.1', port:4243});
+
 console.log('fetching containers');
-docker.listContainers(function (err, containers) {
-  console.log(err, containers);
+docker.listContainers({all: true}, function (err, containers) {
+  var currentTime = Math.floor(Date.now() / 1000); // convert nanoseconds to seconds
+  var deleteContainerStartedBeforeTime = currentTime - MAX_TIME_CONTAINER;
+  var containersToDelete = containers.filter(function(container) {
+    return container.Created < deleteContainerStartedBeforeTime;
+  });
+  console.log('found ' + containersToDelete.length + ' containers.');
+  async.each(containersToDelete, function(container, cb){
+    var Container = docker.getContainer(container.Id);
+    Container.stop(function() {
+      Container.remove(function() {
+        cb();
+      });
+    });
+  }, function(err, results) {
+    console.log(containersToDelete.length + ' containers deleted.');
+  });
 });
