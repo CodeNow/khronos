78b7e793c1188645bd53b6d32bacddc15a94c042
Casey Flynn <cflynn.us@gmail.com>
2014-11-17 15:58:23 -0800

    updating prune containers and test files

--- scripts/prune-containers.js
+++ scripts/prune-containers.js
@@ -1,31 +1,34 @@
-#!/usr/bin/env node
 /**
  * Prunes containers running/dead > 12 hours
+ * from image "docker-image-builder"
  */
 var async = require('async');
 var config = require('../config');
 var Docker = require('dockerode');
 
-console.log('connecting to docker daemon');
-var docker = new Docker({host:config.network.host, port:config.network.port});
+module.exports = function(cb) {
+  console.log('connecting to docker daemon');
+  var docker = new Docker({host:config.network.host, port:config.network.port});
 
-console.log('fetching containers');
-docker.listContainers({all: true}, function (err, containers) {
-  var currentTime = Math.floor(Date.now() / 1000); // convert nanoseconds to seconds
-  var deleteContainerStartedBeforeTime = currentTime - config.settings.maxContainerLiveTime;
-  var containersToDelete = containers.filter(function(container) {
-    return container.Created < deleteContainerStartedBeforeTime &&
-           container.Image === 'docker-image-builder';
-  });
-  console.log('found ' + containersToDelete.length + ' containers.');
-  async.each(containersToDelete, function(container, cb){
-    var Container = docker.getContainer(container.Id);
-    Container.stop(function() {
-      Container.remove(function() {
-        cb();
-      });
+  console.log('fetching containers');
+  docker.listContainers({all: true}, function (err, containers) {
+    var currentTime = Math.floor(Date.now() / 1000); // convert nanoseconds to seconds
+    var deleteContainerStartedBeforeTime = currentTime - config.settings.maxContainerLiveTime;
+    var containersToDelete = containers.filter(function(container) {
+      return container.Created < deleteContainerStartedBeforeTime &&
+             container.Image === 'docker-image-builder';
     });
-  }, function(err, results) {
-    console.log(containersToDelete.length + ' containers deleted.');
+    console.log('found ' + containersToDelete.length + ' containers.');
+    async.each(containersToDelete, function(container, cb){
+      var Container = docker.getContainer(container.Id);
+      Container.stop(function() {
+        Container.remove(function() {
+          cb();
+        });
+      });
+    }, function(err, results) {
+      console.log(containersToDelete.length + ' containers deleted.');
+      cb();
+    });
   });
-});
+};
--- test/prune-containers.unit.js
+++ test/prune-containers.unit.js
@@ -1,19 +1,47 @@
+var async = require('async');
 var chai = require('chai');
 var Lab = require('lab');
 var lab = exports.lab = Lab.script();
+var rewire = require('rewire');
 
 var describe = lab.describe;
 var it = lab.it;
 var before = lab.before;
+var beforeEach = lab.beforeEach;
 var after = lab.after;
+var afterEach = lab.afterEach;
 var expect = chai.expect;
 
+var config = JSON.parse(JSON.stringify(require('../config')));
+config.network.port = 5555;
+
 var dockerMock = require('docker-mock');
+dockerMock.listen(config.network.port);
 
-describe('basic', function() {
-  it('should pass', function(done) {
-    
+var docker = require('dockerode')({host:config.network.host, port:config.network.port});
 
-    done();
+// replace private variables for testing
+var pruneContainers = rewire('../scripts/prune-containers');
+pruneContainers.__set__('config', config);
+
+var mocks = require('./mocks');
+
+describe('prune-containers', function() {
+  var containers = [];
+
+  beforeEach(function(done) {
+    async.forEach(mocks.containers, function(container, cb) {
+      docker.createContainer(container, cb);
+    }, done);
+  });
+
+  afterEach(function(done) {
+    async.map(containers, function(container, cb) {
+      container.remove(cb);
+    }, done);
+  });
+
+  it('should delete containers older than 12 hours', function(done) {
+    pruneContainers(done);
   });
 });
