ab5d0f4152e7c970ead95dac88e48f2a8168d151
Casey Flynn <cflynn.us@gmail.com>
2015-02-04 16:41:47 -0800

    update index to run scripts in order in cron

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 require('loadenv');
+var CronJob = require('cron').CronJob;
+var async = require('async');
 
 // will read command line input
 // and launch multiple scripts
 // eventuallycontainers
-//var pruneOrphanImages = require('./scripts/prune-orphan-images');
-var pruneExpiredImages = require('./scripts/prune-expired-images');
+var pruneOrphanImages = require('./scripts/prune-orphan-images');
+var pruneExpiredContextVersions = require('./scripts/prune-expired-context-versions');
 
-//pruneOrphanImages();
-pruneExpiredImages();
-
-/*
-var CronJob = require('cron').CronJob;
 var job = new CronJob({
   cronTime: '00 00 12 * * 0-6', // every day at 12:00
   onTick: function () {
-    pruneOrphanImages();
+    async.series([
+      pruneExpiredContextVersions,
+      pruneOrphanImages
+    ]);
   },
   start: true, // run immediately
   timeZone: 'America/Los_Angeles'
 });
-*/
