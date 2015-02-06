4c5f844b291bfe6fd3cc6cd176957197e67c283d
Casey Flynn <cflynn.us@gmail.com>
2015-02-05 17:51:05 -0800

    commenting code for clarification

--- scripts/prune-orphan-images.js
+++ scripts/prune-orphan-images.js
@@ -55,25 +55,38 @@ module.exports = function(finalCB) {
             debug.log('fetching context-versions ' + lowerBound + ' - ' + upperBound);
             mongodb.fetchContextVersionsForImages(imageSet, function (err, contextVersions) {
               if (err) { return doWhilstIteratorCB(err); }
+              /**
+               * The difference between the range (upperBound-lowerBound) and the number
+               * of contextVersions that were retrieved is the number of orphaned images
+               * that have just been discovered on the current dock.
+               */
               var numberMissing = (upperBound - lowerBound) - contextVersions.length;
               if (!numberMissing) {
                 debug.log('all images in set '+lowerBound+'-'+upperBound+' found, proceeding...');
                 return doWhilstIteratorCB();
               }
               debug.log(numberMissing + ' images on box not in database, cleaning up...');
+              // track total number of orphaned images that were discovered in this cron iteration
               orphanedImagesCount += numberMissing;
+              // need array of mongids in string format to perform search
               var foundCvIDs = contextVersions.map(function (res) {
                 return res['_id'].toString();
               });
-              // figure out which images in imageSet do not have corresponding context-versions
+              /**
+               * determine which images in imageSet do not have corresponding context-versions
+               * by iterating over each image in imageSet, and searching through the retrieved
+               * context-version documents for a match. If no match found, this image is an
+               * orphan.
+               */
               async.forEach(imageSet,
                 function (image, eachCB) {
                   var imageCVIDEqualsFn = equals(regexImageTagCV.exec(image.RepoTags[0])[2]);
                   if (-1 !== findIndex(foundCvIDs, imageCVIDEqualsFn)) {
-                    // image has corresponding cv, continue
+                    // image has corresponding cv, continue (not orphan)
                     return eachCB();
                   }
                   debug.log('cv not found for image: ' + image.Id);
+                  // orphan found
                   docker.removeImage(image.Id, function (err) {
                     if (err) {
                       debug.log('failed to remove image: '+image.Id+ ' on dock: '+dock.host);
@@ -84,6 +97,10 @@ module.exports = function(finalCB) {
               }, doWhilstIteratorCB);
             });
           }
+          /**
+           * Chunk requests to mongodb to avoid potential memory/heap size issues
+           * when working with large numbers of images and context-version documents
+           */
           async.doWhilst(
             doWhilstIterator,
             function check () {
