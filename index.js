9defffb1753fe4836c6a9fc828d74721fe9e1e3e
Casey Flynn <cflynn.us@gmail.com>
2015-02-11 10:14:24 -0800

    datadog & logging to root file

--- index.js
+++ index.js
@@ -2,16 +2,25 @@ require('loadenv');
 var CronJob = require('cron').CronJob;
 var async = require('async');
 
+var datadog = require('models/datadog/datadog')(__filename);
+var debug = require('models/debug/debug')(__filename);
+
 var pruneOrphanImages = require('./scripts/prune-orphan-images');
 var pruneExpiredContextVersions = require('./scripts/prune-expired-context-versions');
 
 new CronJob({
   cronTime: '00 00 12 * * 0-6', // every day at 12:00
   onTick: function () {
+    var timingKey = 'cron-scripts-duration';
+    debug.log('cron started: ' + new Date().toString());
+    datadog.startTiming(timingKey);
     async.series([
       pruneExpiredContextVersions,
       pruneOrphanImages
-    ]);
+    ], function () {
+      debug.log('cron completed: ' + new Date().toString());
+      datadog.endTiming(timingKey);
+    });
   },
   start: true, // run immediately
   timeZone: 'America/Los_Angeles'
