adad472a83d9b9e96754ddc943e5594de0d240d5
Casey Flynn <cflynn.us@gmail.com>
2015-02-11 10:52:18 -0800

    logging start & exit

--- index.js
+++ index.js
@@ -8,17 +8,23 @@ var debug = require('models/debug/debug')(__filename);
 var pruneOrphanImages = require('./scripts/prune-orphan-images');
 var pruneExpiredContextVersions = require('./scripts/prune-expired-context-versions');
 
+debug.log('khronos started '+new Date().toString());
+
+process.on('exit', function () {
+  debug.log('khronos exit'+new Date().toString());
+});
+
 new CronJob({
   cronTime: '00 00 12 * * 0-6', // every day at 12:00
   onTick: function () {
     var timingKey = 'cron-scripts-duration';
-    debug.log('cron started: ' + new Date().toString());
+    debug.log('cron run started: '+new Date().toString());
     datadog.startTiming(timingKey);
     async.series([
       pruneExpiredContextVersions,
       pruneOrphanImages
     ], function () {
-      debug.log('cron completed: ' + new Date().toString());
+      debug.log('cron run completed: '+new Date().toString());
       datadog.endTiming(timingKey);
     });
   },
