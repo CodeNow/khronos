f40c7e5b63d26ac10fb9ebd80e6550733e36f705
Casey Flynn <cflynn.us@gmail.com>
2015-02-10 16:18:15 -0800

    remove comments

--- test/prune-expired-context-versions.unit.js
+++ test/prune-expired-context-versions.unit.js
@@ -65,103 +65,66 @@ describe('prune-expired-context-versions', function() {
     });
 
     it('should only remove contextversions that fit selection criteria', function (done) {
-      //pruneExpiredContextVersions
-      done();
+      var testData = [{
+        _id: ObjectID('54da9cb6ed4383c43fb1504a'),
+        build: {
+          started: new Date(1980, 1, 1),
+          completed: true,
+          dockerTag: true
+        }
+      }, {
+        _id: ObjectID('54da9cb6ed4383c43fb1504e'),
+        build: {
+          started: new Date(),
+          complete: true,
+          dockerTag: true
+        }
+      }];
+      var cv = db.collection('contextversions');
+      cv.insert(testData, function (err) {
+        if (err) { throw err; }
+        pruneExpiredContextVersions(function () {
+          cv.find({}).toArray(function (err, results) {
+            expect(results.length).to.equal(1);
+            expect(results[0]._id.toString()).to.equal('54da9cb6ed4383c43fb1504e');
+            done();
+          });
+        });
+      });
     });
 
+    /*
     it('should properly restore deleted contextversions when cv '+
        'restored after initial blacklist fetch', function (done) {
+      var testData = [{
+        _id: ObjectID('54da9cb6ed4383c43fb1504a'),
+        build: {
+          started: new Date(1980, 1, 1),
+          completed: true,
+          dockerTag: true
+        }
+      }, {
+        _id: ObjectID('54da9cb6ed4383c43fb1504e'),
+        build: {
+          started: new Date(),
+          complete: true,
+          dockerTag: true
+        }
+      }];
+      var cv = db.collection('contextversions');
+      cv.insert(testData, function (err) {
+        if (err) { throw err; }
+        pruneExpiredContextVersions(function () {
+          cv.find({}).toArray(function (err, results) {
+            expect(results.length).to.equal(1);
+            expect(results[0]._id.toString()).to.equal('54da9cb6ed4383c43fb1504e');
+            done();
+          });
+        });
+      });
       done();
     });
+    */
+
   });
 });
-
-
-
-
-/*
-    describe('no images', function () {
-      it('should run successfully if no images on dock', {timeout: 100000}, function (done) {
-        pruneOrphanImages(function () {
-          expect(Image.prototype.remove.called).to.equal(false);
-          done();
-        });
-      });
-    });
-
-    describe('dock with images', function () {
-      it('should run successfully with no orphaned images on dock', function (done) {
-        var cvs = [];
-        async.series([
-          function createCVs (cb) {
-            var contextVersions = db.collection('contextversions');
-            async.times(4, function (n, cb) {
-              contextVersions.insert({}, function (err, _cv) {
-                cvs.push(_cv[0]);
-                cb();
-              });
-            }, cb);
-          },
-          function createImages (cb) {
-            async.eachLimit(cvs, 1, function (cv, cb) { // bit of a concurrency bug in tests
-              var cvId = cv['_id']+''; // must cast to string
-              docker.createImage({
-                fromImage: 'registry.runnable.com/1616464/'+cvId,
-                tag: cvId
-              }, function (err) {
-                if (err) { throw err; }
-                cb();
-              });
-            }, cb);
-          }
-        ], function (err) {
-          if (err) { throw err; }
-          pruneOrphanImages(function () {
-            expect(Image.prototype.remove.called).to.equal(false);
-            done();
-          });
-        });
-      });
-
-      it('should only remove orphaned images from dock', {timeout: 1000*5}, function (done) {
-        var cvs = [];
-        var orphans = [];
-        async.series([
-          function createCVs (cb) {
-            var contextVersions = db.collection('contextversions');
-            async.times(1, function (n, cb) {
-              contextVersions.insert({}, function (err, _cv) {
-                cvs.push(_cv[0]);
-                cb();
-              });
-            }, cb);
-          },
-          function createImages (cb) {
-            // creating orphans
-            orphans.push({'_id': new ObjectID('999017345affa9400d894407')});
-            orphans.push({'_id': new ObjectID('999015ac341e8eb10b4a0328')});
-            orphans.push({'_id': new ObjectID('999015ac341e8eb10b4a0329')});
-            cvs = cvs.concat(orphans);
-            async.eachLimit(cvs, 1, function (cv, cb) { // bit of a concurrency bug in tests
-              var cvId = cv['_id']+''; // must cast to string
-              docker.createImage({
-                fromImage: 'registry.runnable.com/1616464/'+cvId,
-                tag: cvId
-              }, function (err) {
-                if (err) { throw err; }
-                cb();
-              });
-            }, cb);
-          }
-        ], function (err) {
-          if (err) { throw err; }
-          pruneOrphanImages(function () {
-            expect(Image.prototype.remove.callCount).to.equal(orphans.length);
-            console.log('last test!');
-            done();
-          });
-        });
-      });
-    });
-  });
-*/
