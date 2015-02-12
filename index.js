62e514b5cc7c21cfcd9a4378dac79be4e4f507b3
Casey Flynn <cflynn.us@gmail.com>
2015-02-11 16:58:31 -0800

    manual-run flag

--- index.js
+++ index.js
@@ -14,20 +14,29 @@ process.on('exit', function () {
   debug.log('khronos exit'+new Date().toString());
 });
 
-new CronJob({
-  cronTime: '00 00 12 * * 0-6', // every day at 12:00
-  onTick: function () {
-    var timingKey = 'cron-scripts-duration';
-    debug.log('cron run started: '+new Date().toString());
-    datadog.startTiming(timingKey);
-    async.series([
-      pruneExpiredContextVersions,
-      pruneOrphanImages
-    ], function () {
-      debug.log('cron run completed: '+new Date().toString());
-      datadog.endTiming(timingKey);
-    });
-  },
-  start: true, // run immediately
-  timeZone: 'America/Los_Angeles'
-});
+if (process.env.MANUAL_RUN) {
+  async.series([
+    pruneExpiredContextVersions,
+    pruneOrphanImages
+  ], function () {
+    debug.log('complete '+new Date().toString());
+  });
+} else {
+  new CronJob({
+    cronTime: '00 00 12 * * 0-6', // every day at 12:00
+    onTick: function () {
+      var timingKey = 'cron-scripts-duration';
+      debug.log('cron run started: '+new Date().toString());
+      datadog.startTiming(timingKey);
+      async.series([
+        pruneExpiredContextVersions,
+        pruneOrphanImages
+      ], function () {
+        debug.log('cron run completed: '+new Date().toString());
+        datadog.endTiming(timingKey);
+      });
+    },
+    start: true, // run immediately
+    timeZone: 'America/Los_Angeles'
+  });
+}
