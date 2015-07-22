2aa051f577ce31603d17e5ae5990383d3eab8104
Casey Flynn <cflynn.us@gmail.com>
2015-07-22 15:49:35 -0700

    debug replace bunyan

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ require('loadenv')('khronos:env');
 var CronJob = require('cron').CronJob;
 var async = require('async');
 
-var cron;
 var datadog = require('models/datadog/datadog')(__filename);
-var debug = require('models/debug/debug')(__filename);
+var log = require('logger').getChild(__filename);
 var mongodb = require('models/mongodb/mongodb');
-
 var pruneExitedWeaveContainers = require('./scripts/prune-exited-weave-containers');
 var pruneExpiredContextVersions = require('./scripts/prune-expired-context-versions');
 var pruneImageBuilderContainers = require('./scripts/prune-image-builder-containers');
 var pruneOrphanContainers = require('./scripts/prune-orphan-containers');
 var pruneOrphanImages = require('./scripts/prune-orphan-images');
+
+var cron;
 
 // functions to be run sequentially for both
 // manual run and cron run
@@ -29,17 +29,22 @@ var seriesFunctions = [
   pruneOrphanImages,
   pruneExitedWeaveContainers
 ];
-debug.log('khronos started '+new Date().toString());
+
+log.info('khronos process up');
+
 process.on('exit', function () {
-  debug.log('khronos exit'+new Date().toString());
+  log.info('khronos process exit');
 });
 mongodb.connect(function (err) {
   if (err) {
-    return debug(err);
+    log.error({
+      err: err
+    }, 'mongodb.connect error');
+    return;
   }
   if (process.env.MANUAL_RUN) {
     async.series(seriesFunctions, function () {
-      debug.log('complete '+new Date().toString());
+      log.trace('complete manual run');
       process.exit(0);
     });
   } else {
@@ -47,10 +52,10 @@ mongodb.connect(function (err) {
       cronTime: process.env.KHRONOS_INTERVAL,
       onTick: function () {
         var timingKey = 'cron-scripts-duration';
-        debug.log('cron run started: '+new Date().toString());
+        log.trace('cron run started');
         datadog.startTiming(timingKey);
         async.series(seriesFunctions, function () {
-          debug.log('cron run completed: '+new Date().toString());
+          log.trace('cron run completed');
           datadog.endTiming(timingKey);
         });
       },
