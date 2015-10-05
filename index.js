64f93c9ea832e35a0aca8172280d3e36203b4067
Bryan Kendall <bryan@runnable.com>
2015-10-05 15:26:29 -0700

    removing lib from all paths

--- index.js
+++ index.js
@@ -2,27 +2,26 @@
 
 require('loadenv')('khronos:test');
 
-var log = require('./lib/logger');
+var log = require('logger');
 var ponos = require('ponos');
-var rabbitmq = require('./lib/models/rabbitmq');
+var rabbitmq = require('models/rabbitmq');
 
 var tasks = {
-  'khronos:containers:delete': require('./lib/tasks/containers/delete'),
+  'khronos:containers:delete': require('tasks/containers/delete'),
   'khronos:containers:image-builder:prune':
-    require('./lib/tasks/image-builder/prune'),
+    require('tasks/image-builder/prune'),
   'khronos:containers:image-builder:prune-dock':
-    require('./lib/tasks/image-builder/prune-dock'),
-  'khronos:containers:orphan:prune':
-    require('./lib/tasks/containers/prune-orphans'),
+    require('tasks/image-builder/prune-dock'),
+  'khronos:containers:orphan:prune': require('tasks/containers/prune-orphans'),
   'khronos:containers:orphan:prune-dock':
-    require('./lib/tasks/containers/prune-orphans-dock'),
+    require('tasks/containers/prune-orphans-dock'),
   'khronos:containers:orphan:check-against-mongo':
-      require('./lib/tasks/containers/check-against-mongo'),
-  'khronos:containers:remove': require('./lib/tasks/containers/remove'),
+      require('tasks/containers/check-against-mongo'),
+  'khronos:containers:remove': require('tasks/containers/remove'),
   // 'khronos:context-versions:prune':
   // 'khronos:images:orphan:prune':
-  'khronos:weave:prune-dock': require('./lib/tasks/weave/prune-dock'),
-  'khronos:weave:prune': require('./lib/tasks/weave/prune')
+  'khronos:weave:prune-dock': require('tasks/weave/prune-dock'),
+  'khronos:weave:prune': require('tasks/weave/prune')
 };
 var hermes = rabbitmq(Object.keys(tasks));
 var server = new ponos.Server({ hermes: hermes });
