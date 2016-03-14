4e121f0dff4f759314dcc979b3080a54ca53a058
Ryan Sandor Richards <ryan@runnable.com>
2016-03-14 15:19:40 -0700

    Removed logging pipe and abort to see actual error on start...

---
+++ test/unit/tasks/canary/rebuild.js
@@ -0,0 +1,53 @@
+// 'use strict'
+//
+// require('loadenv')('khronos:test')
+//
+// var chai = require('chai')
+// var assert = chai.assert
+// chai.use(require('chai-as-promised'))
+//
+// // external
+// var Bunyan = require('bunyan')
+// var Hermes = require('runnable-hermes')
+// var sinon = require('sinon')
+// var TaskFatalError = require('ponos').TaskFatalError
+//
+// // internal
+// var MongoDB = require('models/mongodb')
+//
+// // internal (being tested)
+// var rebuildCanary = require('tasks/canary/rebuild')
+//
+// describe('Rebuild Canary', function () {
+//   describe('initializeClient', function () {
+//     it('should connect the API client user', function () {
+//     })
+//   }) // end 'initializeClient'
+//
+//   describe('issueRebuild', function () {
+//     it('should issue a rebuild via the API client', function () {
+//     })
+//   }) // end 'issueRebuild'
+//
+//   describe('fetchResults', function () {
+//     it('should wait until the build has completed or failed', function () {
+//     })
+//   }) // end 'fetchResults'
+//
+//   describe('testRunningContainer', function () {
+//     it('should test the running container and check the result', function () {
+//     })
+//   }) // end 'testRunningContainer'
+//
+//   describe('publishToDatadog', function () {
+//     describe('on success', function () {
+//       it('should publish a pass to datadog', function () {
+//       })
+//     }) // end 'on success'
+//
+//     describe('on failure', function () {
+//       it('should publish a fail to datadog', function () {
+//       })
+//     }) // end 'on failure'
+//   }) // end 'publishToDatadog'
+// }) // end 'Rebuild Canary'
