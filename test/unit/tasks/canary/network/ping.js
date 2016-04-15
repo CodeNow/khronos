b6812e443f21534a28504172df7d36c9d5d04c60
Anandkumar Patel <anand@runnable.com>
2016-04-15 13:00:33 -0700

    add more test

--- test/unit/tasks/canary/network/ping.js
+++ test/unit/tasks/canary/network/ping.js
@@ -1,12 +1,13 @@
 'use strict'
 
-require('loadenv')('khronos:test')
+require('loadenv')()
 
 const chai = require('chai')
 const assert = chai.assert
 chai.use(require('chai-as-promised'))
 
 // external
+const Dockerode = require('dockerode')
 const sinon = require('sinon')
 
 // internal
@@ -19,33 +20,139 @@ const Swarm = require('models/swarm')
 const pingCanary = require('tasks/canary/network/ping')
 
 // TODO anand: flesh out the unit tests for this canary
-describe('Rebuild Canary', () => {
+describe('Network Ping Canary', () => {
   const cleanupQueue = 'khronos:canary:network-cleanup'
   const mock = {
     job: {
-      targetDockerUrl: 'http://1.2.3.4:4242'
+      targetDockerUrl: 'http://1.2.3.4:4242',
+      targetIps: ['10.0.0.1', '10.0.0.2'],
+      targetOrg: 123123
     }
   }
 
+  before(function () {
+    process.env.RUNNABLE_WAIT_FOR_WEAVE = 'wait;for;weave;'
+    process.env.NETWORK_PING_IMAGE = 'runnable/hemingdal'
+  })
+
   beforeEach(() => {
-    sinon.stub(CanaryBase.prototype, 'handleSuccess')
     sinon.stub(CanaryBase.prototype, 'handleCanaryError')
     sinon.stub(CanaryBase.prototype, 'handleGenericError')
-    sinon.stub(Swarm.prototype, 'checkHostExists')
+    sinon.stub(CanaryBase.prototype, 'handleSuccess')
     sinon.stub(Docker.prototype, 'pull')
+    sinon.stub(Dockerode.prototype, 'run')
+    sinon.stub(Hermes.prototype, 'close').yieldsAsync()
+    sinon.stub(Hermes.prototype, 'connect').yieldsAsync()
     sinon.stub(Hermes.prototype, 'publish')
+    sinon.stub(Swarm.prototype, 'checkHostExists')
   })
 
   afterEach(() => {
-    CanaryBase.prototype.handleSuccess.restore()
     CanaryBase.prototype.handleCanaryError.restore()
     CanaryBase.prototype.handleGenericError.restore()
-    Swarm.prototype.checkHostExists.restore()
+    CanaryBase.prototype.handleSuccess.restore()
     Docker.prototype.pull.restore()
+    Dockerode.prototype.run.restore()
+    Hermes.prototype.close.restore()
+    Hermes.prototype.connect.restore()
     Hermes.prototype.publish.restore()
+    Swarm.prototype.checkHostExists.restore()
   })
 
+  after(function (done) {
+    delete process.env.RUNNABLE_WAIT_FOR_WEAVE
+    delete process.env.NETWORK_PING_IMAGE
+    done()
+  })
+
+  describe('invalid job', () => {
+    it('should throw TaskFatalError', () => {
+      return pingCanary({}).then(() => {
+        sinon.assert.calledOnce(CanaryBase.prototype.handleGenericError)
+      })
+    })
+
+    it('should throw TaskFatalError', () => {
+      return pingCanary({
+        targetDockerUrl: '10.0.0.1:4242',
+        targetIps: ['10.0.0.1'],
+        targetOrg: 123
+      }).then(() => {
+        sinon.assert.calledOnce(CanaryBase.prototype.handleGenericError)
+      })
+    })
+
+    it('should throw TaskFatalError', () => {
+      return pingCanary({
+        targetDockerUrl: '',
+        targetIps: ['10.0.0.1'],
+        targetOrg: 123
+      }).then(() => {
+        sinon.assert.calledOnce(CanaryBase.prototype.handleGenericError)
+      })
+    })
+
+    it('should throw TaskFatalError', () => {
+      return pingCanary({
+        targetDockerUrl: 'http://10.0.0.1:4242',
+        targetIps: [1, 2],
+        targetOrg: 123
+      }).then(() => {
+        sinon.assert.calledOnce(CanaryBase.prototype.handleGenericError)
+      })
+    })
+
+    it('should throw TaskFatalError', () => {
+      return pingCanary({
+        targetDockerUrl: 'http://10.0.0.1:4242',
+        targetIps: ['a', 'b'],
+        targetOrg: 123
+      }).then(() => {
+        sinon.assert.calledOnce(CanaryBase.prototype.handleGenericError)
+      })
+    })
+
+    it('should throw TaskFatalError', () => {
+      return pingCanary({
+        targetDockerUrl: 'http://10.0.0.1:4242',
+        targetIps: ['10.0.0.1', [1]],
+        targetOrg: 123
+      }).then(() => {
+        sinon.assert.calledOnce(CanaryBase.prototype.handleGenericError)
+      })
+    })
+
+    it('should throw TaskFatalError', () => {
+      return pingCanary({
+        targetDockerUrl: 'http://10.0.0.1:4242',
+        targetIps: ['10.0.0.1'],
+        targetOrg: 'org'
+      }).then(() => {
+        sinon.assert.calledOnce(CanaryBase.prototype.handleGenericError)
+      })
+    })
+  }) // end invalid job
+
   describe('on success', () => {
+    beforeEach(() => {
+      Swarm.prototype.checkHostExists.resolves()
+      Docker.prototype.pull.resolves()
+    })
+
+    it('should checkHostExists', () => {
+      return pingCanary(mock.job).then(() => {
+        sinon.assert.calledOnce(Swarm.prototype.checkHostExists)
+        sinon.assert.calledWith(Swarm.prototype.checkHostExists, mock.job.targetDockerUrl)
+      })
+    })
+
+    it('should pull image', () => {
+      return pingCanary(mock.job).then(() => {
+        sinon.assert.calledOnce(Docker.prototype.pull)
+        sinon.assert.calledWith(Docker.prototype.pull, process.env.NETWORK_PING_IMAGE)
+      })
+    })
+
     it('should enqueue the cleanup task', () => {
       return pingCanary(mock.job).then(() => {
         sinon.assert.calledOnce(Hermes.prototype.publish)
