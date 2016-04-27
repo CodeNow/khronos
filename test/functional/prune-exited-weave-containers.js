3f0f7aeafd25a32c55993ff47cdbf84e1bb19588
Anton Podviaznikov <podviaznikov@gmail.com>
2016-04-27 12:38:12 -0700

    experimenting with the tests

--- test/functional/prune-exited-weave-containers.js
+++ test/functional/prune-exited-weave-containers.js
@@ -56,8 +56,8 @@ describe('Prune Exited Weave Containers', function () {
       .persist()
       .get('/v1/kv/swarm/docker/swarm/nodes/?recurse=true')
       .reply(200, [
-        { Key: 'swarm/docker/swarm/nodes/localhost:4242',
-          Value: 'localhost:4242' },
+        { Key: 'swarm/docker/swarm/nodes/localhost:5454',
+          Value: 'localhost:5454' },
       ])
   })
   beforeEach(function () {
@@ -103,83 +103,83 @@ describe('Prune Exited Weave Containers', function () {
     })
   })
 
-  // describe('on a populated dock', function () {
-  //   beforeEach(dockerFactory.createRandomContainers.bind(null, docker, 5))
-  //
-  //   it('should run successfully with no weave containers', function (done) {
-  //     workerServer.hermes.publish('khronos:weave:prune', {})
-  //     async.doUntil(
-  //       function (cb) { setTimeout(cb, 100) },
-  //       function () {
-  //         return tasks['khronos:weave:prune-dock'].callCount === 1
-  //       },
-  //       function (err) {
-  //         if (err) { return done(err) }
-  //         expect(Container.prototype.remove.callCount).to.equal(0)
-  //         dockerFactory.listContainersAndAssert(
-  //           docker,
-  //           function (containers) { expect(containers).to.have.length(5) },
-  //           function (err) {
-  //             if (err) { return done(err) }
-  //             setTimeout(done, 100)
-  //           })
-  //       })
-  //   })
-  //   describe('with multiple docks', function () {
-  //     beforeEach(function () {
-  //       nock.cleanAll()
-  //       nock('http://localhost:4242', { allowUnmocked: true })
-  //         .persist()
-  //         .get('/info')
-  //         .reply(200, swarmInfoMock([{
-  //           host: 'localhost:5454'
-  //         }, {
-  //           host: 'localhost:5454'
-  //         }]))
-  //     })
-  //     it('should run successfully', function (done) {
-  //       workerServer.hermes.publish('khronos:weave:prune', {})
-  //       async.doUntil(
-  //         function (cb) { setTimeout(cb, 100) },
-  //         function () {
-  //           return tasks['khronos:weave:prune-dock'].callCount === 2
-  //         },
-  //         function (err) {
-  //           if (err) { return done(err) }
-  //           expect(Container.prototype.remove.callCount).to.equal(0)
-  //           dockerFactory.listContainersAndAssert(
-  //             docker,
-  //             function (containers) { expect(containers).to.have.length(5) },
-  //             function (err) {
-  //               if (err) { return done(err) }
-  //               setTimeout(done, 100)
-  //             })
-  //         })
-  //     })
-  //   })
-  //
-  //   describe('where weave containers are present', function () {
-  //     beforeEach(dockerFactory.createWeaveContainers.bind(null, docker, 2))
-  //
-  //     it('should only remove dead weave containers', function (done) {
-  //       workerServer.hermes.publish('khronos:weave:prune', {})
-  //       async.doUntil(
-  //         function (cb) { setTimeout(cb, 100) },
-  //         function () { return Container.prototype.remove.callCount === 2 },
-  //         function (err) {
-  //           if (err) { return done(err) }
-  //           expect(tasks['khronos:weave:prune-dock'].callCount).to.equal(1)
-  //           expect(tasks['khronos:containers:delete'].callCount).to.equal(2)
-  //           expect(Container.prototype.remove.callCount).to.equal(2)
-  //           dockerFactory.listContainersAndAssert(
-  //             docker,
-  //             function (containers) { expect(containers).to.have.length(5) },
-  //             function (err) {
-  //               if (err) { return done(err) }
-  //               setTimeout(done, 100)
-  //             })
-  //         })
-  //     })
-  //   })
-  // })
+  describe('on a populated dock', function () {
+    beforeEach(dockerFactory.createRandomContainers.bind(null, docker, 5))
+
+    it('should run successfully with no weave containers', function (done) {
+      workerServer.hermes.publish('khronos:weave:prune', {})
+      async.doUntil(
+        function (cb) { setTimeout(cb, 100) },
+        function () {
+          return tasks['khronos:weave:prune-dock'].callCount === 1
+        },
+        function (err) {
+          if (err) { return done(err) }
+          expect(Container.prototype.remove.callCount).to.equal(0)
+          dockerFactory.listContainersAndAssert(
+            docker,
+            function (containers) { expect(containers).to.have.length(5) },
+            function (err) {
+              if (err) { return done(err) }
+              setTimeout(done, 100)
+            })
+        })
+    })
+    describe('with multiple docks', function () {
+      beforeEach(function () {
+        nock.cleanAll()
+        nock('http://localhost:4242', { allowUnmocked: true })
+          .persist()
+          .get('/info')
+          .reply(200, swarmInfoMock([{
+            host: 'localhost:5454'
+          }, {
+            host: 'localhost:5454'
+          }]))
+      })
+      it('should run successfully', function (done) {
+        workerServer.hermes.publish('khronos:weave:prune', {})
+        async.doUntil(
+          function (cb) { setTimeout(cb, 100) },
+          function () {
+            return tasks['khronos:weave:prune-dock'].callCount === 2
+          },
+          function (err) {
+            if (err) { return done(err) }
+            expect(Container.prototype.remove.callCount).to.equal(0)
+            dockerFactory.listContainersAndAssert(
+              docker,
+              function (containers) { expect(containers).to.have.length(5) },
+              function (err) {
+                if (err) { return done(err) }
+                setTimeout(done, 100)
+              })
+          })
+      })
+    })
+
+    describe('where weave containers are present', function () {
+      beforeEach(dockerFactory.createWeaveContainers.bind(null, docker, 2))
+
+      it('should only remove dead weave containers', function (done) {
+        workerServer.hermes.publish('khronos:weave:prune', {})
+        async.doUntil(
+          function (cb) { setTimeout(cb, 100) },
+          function () { return Container.prototype.remove.callCount === 2 },
+          function (err) {
+            if (err) { return done(err) }
+            expect(tasks['khronos:weave:prune-dock'].callCount).to.equal(1)
+            expect(tasks['khronos:containers:delete'].callCount).to.equal(2)
+            expect(Container.prototype.remove.callCount).to.equal(2)
+            dockerFactory.listContainersAndAssert(
+              docker,
+              function (containers) { expect(containers).to.have.length(5) },
+              function (err) {
+                if (err) { return done(err) }
+                setTimeout(done, 100)
+              })
+          })
+      })
+    })
+  })
 })
