c9f610416884c92ca37154fb15ce09fa6e997cdd
Nathan219 <Nathan219@gmail.com>
2016-06-24 16:33:48 -0700

    Change name to organization.created

--- test/unit/tasks/docks/organization-created.js
+++ test/unit/tasks/docks/organization-created.js
@@ -8,21 +8,20 @@ const monitor = require('monitor-dog')
 const sinon = require('sinon')
 
 // Internal
-const rawDocks = require('../../../mocks/swarm/multiple-docks.json')
 const Swarm = require('models/swarm')
 const TaskError = require('ponos').TaskError
 const TaskFatalError = require('ponos').TaskFatalError
 
 // internal (being tested)
-const UserWhitelisted = require('tasks/docks/user-whitelisted')
+const OrganizationCreated = require('tasks/organization/created')
 
 const assert = chai.assert
 chai.use(require('chai-as-promised'))
 require('sinon-as-promised')(require('bluebird'))
 
-describe('User Whitelisted Task', function () {
+describe('Organization Created Task', function () {
   beforeEach(function () {
-    sinon.stub(Swarm.prototype, 'getHostsWithOrgs').resolves(rawDocks)
+    sinon.stub(Swarm.prototype, 'getHostsWithOrgs')
     sinon.stub(monitor, 'event')
   })
   afterEach(function () {
@@ -32,14 +31,14 @@ describe('User Whitelisted Task', function () {
 
   describe('Joi validation', function () {
     it('should fail if empty', function () {
-      return assert.isRejected(UserWhitelisted())
+      return assert.isRejected(OrganizationCreated())
         .then(function (err) {
           assert.instanceOf(err, TaskFatalError)
           assert.include(err.message, 'Invalid Job')
         })
     })
     it('should fail missing createdAt', function () {
-      return assert.isRejected(UserWhitelisted({
+      return assert.isRejected(OrganizationCreated({
         githubId: 123213,
         orgName: 'asdasdasd'
       }))
@@ -49,7 +48,7 @@ describe('User Whitelisted Task', function () {
         })
     })
     it('should fail missing githubId', function () {
-      return assert.isRejected(UserWhitelisted({
+      return assert.isRejected(OrganizationCreated({
         createdAt: Math.floor(new Date().getTime() / 1000),
         orgName: 'asdasdasd'
       }))
@@ -59,7 +58,7 @@ describe('User Whitelisted Task', function () {
         })
     })
     it('should fail missing orgName', function () {
-      return assert.isRejected(UserWhitelisted({
+      return assert.isRejected(OrganizationCreated({
         createdAt: Math.floor(new Date().getTime() / 1000),
         githubId: 123213
       }))
@@ -72,7 +71,7 @@ describe('User Whitelisted Task', function () {
   describe('testing the delay', function () {
     process.env.CHECK_ASG_CREATED_DELAY_IN_SEC = 100
     it('should throw TaskError when not enough time has passed ', function () {
-      return assert.isRejected(UserWhitelisted({
+      return assert.isRejected(OrganizationCreated({
         createdAt: Math.floor(new Date().getTime() / 1000) + 100,
         githubId: 1232132,
         orgName: 'asdasdasd'
@@ -97,14 +96,14 @@ describe('User Whitelisted Task', function () {
     })
 
     it('should resolve successfully', function () {
-      return UserWhitelisted({
+      return OrganizationCreated({
         createdAt: Math.floor(new Date().getTime() / 1000) - 101,
         githubId: orgWithDock,
         orgName: 'asdasdasd'
       })
     })
     it('should fire off a datadog when the org doesn\'t have a dock!', function () {
-      return assert.isRejected(UserWhitelisted({
+      return assert.isRejected(OrganizationCreated({
         createdAt: Math.floor(new Date().getTime() / 1000) - 101,
         githubId: orgWithoutDock,
         orgName: 'asdasdasd'
